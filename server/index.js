17c73654f9c0ef75ed50c196552d6f7e10ded7d7
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-01-30 16:38:22 +0530

    create model folder  and signup api

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
 dotenv.config();
+import User from './models/user.js';
 
 const app = express();
 app.use(express.json());
@@ -13,7 +13,33 @@ mongoose.connect(process.env.MONGODB_URL,()=>{
     console.log('Connected to MongoDB');
 })
 
+// api routes starts here
+app.post('/signup', async (req, res)=>{
+   const {name, phone, email, password, role} = req.body;
 
+   if(!name || !phone || !email || !password || !role){
+    return res.json({
+        success:false,
+        message:"All fields are required"
+    })
+   }
+
+   const user = new User({
+    name: name,
+    phone: phone,
+    email: email,
+    password: password,
+    role: role
+   })
+
+   const savedUser = await user.save();
+
+   res.send({
+    success: true,
+    message: "User created successfully...",
+    data: savedUser
+   })
+})
 
 app.listen(5000,()=>{
     console.log(`Server is running on port ${PORT}`);
