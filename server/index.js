2af96322370ccfc44bb468bd521a0616c7f708f3
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-01-30 22:28:03 +0530

    validations added in sign api

--- server/index.js
+++ server/index.js
@@ -17,12 +17,63 @@ mongoose.connect(process.env.MONGODB_URL,()=>{
 app.post('/signup', async (req, res)=>{
    const {name, phone, email, password, role} = req.body;
 
-   if(!name || !phone || !email || !password || !role){
+//    if(!name || !phone || !email || !password || !role){
+//     return res.json({
+//         success:false,
+//         message:"All fields are required"
+//     })
+//    }
+
+// or
+
+// if(!name){
+//     return res.json({
+//         success:false,
+//         message: "Name is required"
+//     })
+// }
+// if(!phone){
+//     return res.json({
+//         success:false,
+//         message: "Phone is required"
+//     })
+// }
+// if(!email){
+//     return res.json({
+//         success:false,
+//         message: "Email is required"
+//     })
+// }
+// if(!password){
+//     return res.json({
+//         success:false,
+//         message: "Password is required"
+//     })
+// }
+// if(!role){
+//     return res.json({
+//         success:false,
+//         message: "Role` is required"
+//     })
+// }
+
+ // or
+
+ const emptyFields = [];
+
+ if(!name) emptyFields.push('name');
+ if(!phone) emptyFields.push('phone');
+ if(!email) emptyFields.push('email');
+ if(!password) emptyFields.push('password');
+ if(!role) emptyFields.push('role');
+
+ if(emptyFields.length > 0){
     return res.json({
-        success:false,
-        message:"All fields are required"
+        success: false,
+        message: `${emptyFields.join(', ')} are required`
     })
-   }
+ }
+
 
    const user = new User({
     name: name,
