5c0eb76864bc62ae33162cf8f237d6894fa18cf3
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-02-01 13:51:30 +0530

    create model of Table in server

---
+++ server/models/Table.js
@@ -0,0 +1,10 @@
+import { Schema,model } from "mongoose";
+
+const tableSchema = new Schema({
+    tableNumber: Number,
+    occupied: Boolean,
+    occupiedBy: String
+})
+
+const Table = model("Table", tableSchema);
+export default Table;
